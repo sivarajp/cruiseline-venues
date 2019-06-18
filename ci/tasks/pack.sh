8de0bb295ba4f87a277b97796b0dbcd2b93da303
Michael Wright <michaelwright@Michaels-iMac.local>
2019-06-18 16:21:47 -0400

    fix pks pipeline

--- ci/tasks/pack.sh
+++ ci/tasks/pack.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 cd venue
+pack set-default-builder cloudfoundry/cnb:bionic
 pack build venue -p .
 docker tag venue gcr.io/fe-mwright/cruiseline/venue:latest
 docker push gcr.io/fe-mwright/cruiseline/venue:latest
