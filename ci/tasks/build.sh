24fbb795ddba5cbf1617849b870d90c6ead0abf6
Michael Wright <michaelwright@Michaels-iMac.local>
2019-06-18 15:08:12 -0400

    fix build task

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-cruiseline-venue/mvnw package
-cp cruiseline-venue/target/venue-0.0.1-SNAPSHOT.jar build-output/
+venue/mvnw package
+cp venue/target/venue-0.0.1-SNAPSHOT.jar build-output/
