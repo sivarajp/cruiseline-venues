68cf36cc9a99bd39b8c08137ffeaa6c3f917d33a
mwright <mwright@pivotal.io>
2019-06-20 21:06:00 -0400

    change k8s deploy from resource to task

--- ci/tasks/deploy-to-k8s.sh
+++ ci/tasks/deploy-to-k8s.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 kubectl --kubeconfig=build-output/kube-config get pods
+kubectl --kubeconfig=build-output/kube-config delete -f build-output/deployment.yaml
 kubectl --kubeconfig=build-output/kube-config create -f build-output/deployment.yaml
