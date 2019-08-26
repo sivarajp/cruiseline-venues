479c4aec4995741c57d329e251188ebac146c0b5
mwright <mwright@pivotal.io>
2019-08-26 15:11:35 -0400

    update k8s deploy

--- ci/tasks/deploy-to-k8s.sh
+++ ci/tasks/deploy-to-k8s.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-kubectl --kubeconfig=build-output/kube-config get pods
-kubectl --kubeconfig=build-output/kube-config delete -f build-output/deployment.yaml --ignore-not-found=true
-kubectl --kubeconfig=build-output/kube-config create -f build-output/deployment.yaml
+kubectl get pods
+kubectl delete -f venues/deployment.yaml --ignore-not-found=true
+kubectl create -f venues/deployment.yaml
