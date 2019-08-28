77e0f545dcdeef53fe7a299af2ab94332a9b50d9
mwright <mwright@pivotal.io>
2019-08-28 14:19:49 -0400

    cach maven

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -e
+export ROOT_FOLDER=$( pwd )
+export REPO=repo
+
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${ROOT_FOLDER}/maven"
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+
 cd venues
 mvn package
 cp target/venue-0.0.1-SNAPSHOT.jar ../build-output/
