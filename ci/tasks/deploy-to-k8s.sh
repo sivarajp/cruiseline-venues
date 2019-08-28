bcf8576dfee85a9522caf55b4a3bbee01936bcf4
mwright <mwright@pivotal.io>
2019-08-28 13:44:26 -0400

    kapp deploy

--- ci/tasks/deploy-to-k8s.sh
+++ ci/tasks/deploy-to-k8s.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+kubectl config use-context ${CONTEXT}
 kubectl get pods
 kubectl delete -f venues/deployment.yaml --ignore-not-found=true
 kubectl create -f venues/deployment.yaml
---
+++ ci/tasks/kapp.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+mkdir -p ~/.kube
+set +x
+echo "$KUBECONFIG" > ~/.kube/config
+set -x
+kapp deploy -a venues -f venues/k8s/deployment.yaml
