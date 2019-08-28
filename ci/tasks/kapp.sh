04a8652901b4b0741085a47e75a7323d7a271d03
mwright <mwright@pivotal.io>
2019-08-28 16:00:26 -0400

    fix kubeconfig

--- ci/tasks/kapp.sh
+++ ci/tasks/kapp.sh
@@ -2,6 +2,8 @@
 set -e
 mkdir -p ~/.kube
 set +x
+echo "------- KUBECONFIG ---------------------"
+echo "$KUBECONFIG" 
 echo "$KUBECONFIG" > ~/.kube/config
 set -x
 kapp deploy -a venues -f venues/k8s/deployment.yaml
